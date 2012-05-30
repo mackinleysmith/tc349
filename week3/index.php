4d656c97ad8001d48af6abc82e0c35a1734f35fd
MacKinley Smith <smit1625@msu.edu>
2012-05-30 07:22:23 -0400

    Week 3 Complete

---
+++ week3/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="Author" content="MacKinley Smith">
+		<title>TC349 Week 3: Creative Floating</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	</head>
+	<body>
+		<h1>TC349 Week 3: Creative Floating</h1>
+		<article id="sweden">
+			<dl>
+				<dt>Stockholm</dt>
+				<dd class="img"><img src="images/gamlastan.jpg" width="80" height="80" alt="Gamla Stan" /></dd>
+				<dd>This was taken in Gamla Stan (Old Town) in a large square of amazing buildings.</dd>
+			</dl>
+			<dl class="alt">
+				<dt>Gamla Uppsala</dt>
+				<dd class="img"><img src="images/uppsala.jpg" width="80" height="80" alt="Gamla Uppsala" /></dd>
+				<dd>The first three Swedish kings are buried here, under ancient burial mounds.</dd>
+			</dl>
+			<dl>
+				<dt>Perpetual Sun</dt>
+				<dd class="img"><img src="images/watch.jpg" width="80" height="80" alt="Wristwatch" /></dd>
+				<dd>During the summer months, the sun takes forever to go down.  This is a good thing.</dd>
+			</dl>
+		</article>
+	</body>
+</html>
