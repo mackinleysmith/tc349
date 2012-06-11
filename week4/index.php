c48239414880ed382738ee2677a48c3f3c12fa7d
MacKinley Smith <smit1625@msu.edu>
2012-06-11 17:10:18 -0400

    Week 4 Complete

---
+++ week4/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="Author" content="MacKinley Smith">
+		<meta name="viewport" content="width=device-width" />
+		<title>TC349 Week 4: Bulletproof Pretzel Co. Website</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+	</head>
+	<body>
+		<div id="special">
+			<h2><strong>This Week&#8217;s Special:</strong> 30% off all week-old (but still delicious) pretzels!</h2>
+		</div>
+		<div id="wrap">
+			<header role="banner">
+				<div id="logo">
+					<a href="index.php"><img src="images/logo-mark.png" alt="Bulletproof Pretzel Co." /></a>
+				</div>
+				<nav role="navigation">
+					<ul>
+						<li class="active"><a href="#"><strong>Home <em>sweet home</em></strong></a></li>
+						<li><a href="#"><strong>About <em>company history</em></strong></a></li>
+						<li><a href="#"><strong>Pretzels <em>our products</em></strong></a></li>
+						<li><a href="#"><strong>Shop <em>pretzels to go</em></strong></a></li>
+					</ul>
+				</nav>
+			</header>
+			<div id="main" role="main">
+				<h1><strong>BULLETPROOF PRETZEL CO.</strong> is a premier baker of homemade pretzels since 1918. Our secret family recipe has been handed down four generations. Literally. Like on an index card.</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <a href="#">exercitation ullamco</a> laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+				<figure><img src="images/pretzels.jpg" alt="pretzels" /></figure>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div id="sidebar" role="complementary">
+				<div class="callout">
+					<h3>What are Pretzels?</h3>
+					<p>Pretzels are delicious baked snacks made from dough that&#8217;s twisted into shape. They go great with mustard and beer.</p>
+				</div>
+				<h3>Our Pretzels</h3>
+				<ul>
+					<li><a href="#">Plain</a></li>
+					<li><a href="#">Salted</a></li>
+					<li><a href="#">Chocolate covered</a></li>
+					<li><a href="#">Yogurt covered</a></li>
+					<li><a href="#">BBQ</a></li>
+					<li><a href="#">Tofu</a></li>
+				</ul>
+				<h3>About Us</h3>
+				<ul>
+					<li><a href="#">History</a></li>
+					<li><a href="#">Jobs</a></li>
+					<li><a href="#">Contact Us</a></li>
+					<li><a href="#">Directions</a></li>
+				</ul>
+				<h3>Salt &amp; Battery</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<footer role="contentinfo">
+				<p>Copyright &copy; 2011 Bulletproof Pretzel Company.  All rights reserved.</p>
+				<p id="footer-logo"><a href="http://simplebits.com"><img src="images/sb-black.png" /></a></p>
+			</footer>
+		</div>
+	</body>
+</html>
